e4a09a4b614b4af8940b7f96cdca7f77c1f7461b
dgleyramos1 <dgleysilva@gmail.com>
2021-06-29 09:11:52 -0300

    First  commit

---
+++ src/views/components/footer/index.js
@@ -0,0 +1,15 @@
+import Planet from '../../../assets/images/elements/planet.png';
+
+let footer = `
+    <div class="container footer-contain">
+        <p>
+            Star wars  -  este é um exemplo criado para os Alunos Gama Academy
+            Módulo de boas práticas.
+        </p>
+    
+        <img src=${Planet} alt="planet" height="180px" width="auto">
+    </div>
+
+`
+
+export default footer;
